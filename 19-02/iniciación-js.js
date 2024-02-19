1f374167bab9e1aa37a9f4560d45da5810f35cc9
tomacoo13 <juank133@hotmail.es>
2024-02-19 19:56:15 +0100

    subida

---
+++ 19-02/iniciación-js.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 7;
+let aux = b;
+
+b = a
+a = aux
+
+
+// ... aquí y sólo aquí añadiremos las líneas de codigo
+console.log('a: ', a); //Debería mostrar 7
+console.log('b: ', b); //Debería mostrar 5
